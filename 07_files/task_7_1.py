24441879a7a253764f45aa9a7bda17319dd251c0
dkasyan <dmitriy.kasyan@gmail.com>
2022-01-20 17:47:55 +0000

    finish task_7_1

--- 07_files/task_7_1.py
+++ 07_files/task_7_1.py
@@ -14,3 +14,38 @@ Outbound Interface    FastEthernet0/0
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+result = {
+    'Prefix' : None,
+    'AD/Metric' : None,
+    'Next-Hop' : None,
+    'Last update' : None,
+    'Outbound Interface' : None
+}
+
+with open('ospf.txt','r') as f:
+    for line in f:
+### use vars
+#       pref=line.split()[1]
+#       metric=line.split()[2].strip('[]')
+#       hop=line.split()[4][:-1]
+#       upd=line.split()[5][:-1]
+#       intf=line.split()[6]
+
+#         print(f'''
+# prefix {pref:<12}
+# AD/Metric {metric:<12}
+# Next-Hop {hop:<12}
+# Last update {upd:<12}
+# Outbound Interface {intf:<12}
+#             ''')
+### use dict
+        result.update({'Prefix': line.split()[1]})
+        result.update({'AD/Metric' : line.split()[2].strip('[]')})
+        result.update({'Next-Hop' : line.split()[4][:-1]})
+        result.update({'Last update' : line.split()[5][:-1]})
+        result.update({'Outbound Interface' : line.split()[6]})
+        
+        for i, j in result.items():
+            print (f'{i:20} {j:<}')
+        print ('\n')
