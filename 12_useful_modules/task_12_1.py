d517d0d82a0e373e82fb8851146b6497903a9821
dkasyan <dmitriy.kasyan@gmail.com>
2022-06-06 19:04:52 +0000

    2202-06-06 change def in task_12_1 def & prog tuple_of_lists

--- 12_useful_modules/task_12_1.py
+++ 12_useful_modules/task_12_1.py
@@ -18,8 +18,33 @@ IP-адрес считается доступным, если выполнени
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 """
 
-#def ping_ip_addresses(ip_address):
-#
+def ping_ip_addresses(check_ip_address):
+
+    import subprocess
+
+    ip_alive = []
+    ip_unreacheble = []
+
+    for i in check_ip_addresses:
+        my_ping=['ping', '-c', '3', i]
+
+        reply = subprocess.run(my_ping,stdout=subprocess.DEVNULL)
+
+        if reply.returncode == 0:
+            ip_alive.append(i)
+        elif reply.returncode != 0:
+            ip_unreacheble.append(i)
+
+    ping_status = (ip_alive,ip_unreacheble)
+    return ping_status
+
+
+
+
+check_ip_addresses=['9.9.9.9','8.8.8.8','192.168.0.1']
+result = ping_ip_addresses(check_ip_addresses)
+print (result)
+
 #    import subprocess
 #
 #    ip_alive = []
@@ -48,28 +73,28 @@ IP-адрес считается доступным, если выполнени
 #print (result)
 
 
+#######
 
-
-import subprocess
-
-check_ip_addresses=['9.9.9.9','8.8.8.8','192.168.0.1']
-
-ip_alive = []
-ip_unreacheble = []
-
-for i in check_ip_addresses:
-    my_ping=['ping', '-c', '4', i]
-
-    reply = subprocess.run(my_ping,stdout=subprocess.DEVNULL)
-
-    if reply.returncode == 0:
-        ip_alive.append(i)
-    elif reply.returncode != 0:
-        ip_unreacheble.append(i)
-
-print (ip_alive)
-print (ip_unreacheble)
-
-ping_status = (ip_alive,ip_unreacheble)
-print (ping_status)
+#import subprocess
+#
+#check_ip_addresses=['9.9.9.9','8.8.8.8','192.168.0.1']
+#
+#ip_alive = []
+#ip_unreacheble = []
+#
+#for i in check_ip_addresses:
+#    my_ping=['ping', '-c', '4', i]
+#
+#    reply = subprocess.run(my_ping,stdout=subprocess.DEVNULL)
+#
+#    if reply.returncode == 0:
+#        ip_alive.append(i)
+#    elif reply.returncode != 0:
+#        ip_unreacheble.append(i)
+#
+#print (ip_alive)
+#print (ip_unreacheble)
+#
+#ping_status = (ip_alive,ip_unreacheble)
+#print (ping_status)
 
