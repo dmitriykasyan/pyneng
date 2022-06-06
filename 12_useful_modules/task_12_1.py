5081b7f505c9f7f9a847b78aad7f1e04118801ca
dkasyan <dmitriy.kasyan@gmail.com>
2022-06-06 19:10:19 +0000

    2202-06-06 complite task_12_1

--- 12_useful_modules/task_12_1.py
+++ 12_useful_modules/task_12_1.py
@@ -42,59 +42,8 @@ def ping_ip_addresses(check_ip_address):
 
 
 check_ip_addresses=['9.9.9.9','8.8.8.8','192.168.0.1']
-result = ping_ip_addresses(check_ip_addresses)
-print (result)
 
-#    import subprocess
-#
-#    ip_alive = []
-#    ip_unreacheble = []
-#
-#    for i in ip_address:
-#        my_ping=['ping', '-c', '4', i]
-#
-#        reply = subprocess.run(my_ping,stdout=subprocess.DEVNULL)
-#
-#        if reply.returncode == 0:
-#            ip_alive.append
-##           ping_status = 'Alive'
-#        elif reply.returncode != 0:
-##           ping_status ='Unreachable'
-#            ip_unreacheble.append
-#        print ('in def' + ip_address, ip_unreacheble)
-#    return ip_address, ip_unreacheble
-#
-#
-#check_ip_addresses=['8.8.8.8','192.168.0.1']
-#
 #result = ping_ip_addresses(check_ip_addresses)
-## result = ping_ip_addresses('192.168.1.1')
-#print ('Host is ' + result)
 #print (result)
 
-
-#######
-
-#import subprocess
-#
-#check_ip_addresses=['9.9.9.9','8.8.8.8','192.168.0.1']
-#
-#ip_alive = []
-#ip_unreacheble = []
-#
-#for i in check_ip_addresses:
-#    my_ping=['ping', '-c', '4', i]
-#
-#    reply = subprocess.run(my_ping,stdout=subprocess.DEVNULL)
-#
-#    if reply.returncode == 0:
-#        ip_alive.append(i)
-#    elif reply.returncode != 0:
-#        ip_unreacheble.append(i)
-#
-#print (ip_alive)
-#print (ip_unreacheble)
-#
-#ping_status = (ip_alive,ip_unreacheble)
-#print (ping_status)
-
+print (ping_ip_addresses(check_ip_addresses))
