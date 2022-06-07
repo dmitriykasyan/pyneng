33f92176d4d42010ddd885a439e5f31f79a0ee70
dkasyan <dmitriy.kasyan@gmail.com>
2022-06-07 03:56:26 +0000

    2022-06-07 task_12_2 parse_last_octet

--- 12_useful_modules/task_12_2.py
+++ 12_useful_modules/task_12_2.py
@@ -34,3 +34,22 @@
  '172.21.41.129', '172.21.41.130', '172.21.41.131', '172.21.41.132']
 
 """
+
+#ip_range = ['192.168.12.1','10.10.0.1-10']
+
+#for ip_addr in ip_range:
+
+ip_addr = '10.20.30.8-10'
+
+oct1,oct2,oct3,oct4 = ip_addr.split('.')
+
+if '-' in  oct4:
+    print ('oct4 is range')
+    range_start,range_end = oct4.split('-')
+    print ('range from '+range_start+' to '+range_end)
+#   print (list(range(int(range_start),int(range_end)+1)))
+    for oct4_r in range(int(range_start),int(range_end)+1):
+        print (oct1+'.'+oct2+'.'+oct3+'.'+str(oct4_r))
+else:
+    print ('oct4 is`t range')
+    print (ip_addr)
