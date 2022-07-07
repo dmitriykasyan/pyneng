c6764907a133fafd58e37a7c8882646d82c5d125
dkasyan <dmitriy.kasyan@gmail.com>
2022-07-07 03:01:17 +0000

    2022-07-07 task_15_1b_change_regexp

--- 15_module_re/task_15_1b.py
+++ 15_module_re/task_15_1b.py
@@ -28,3 +28,50 @@ IP-адреса, диапазоны адресов и так далее, так 
 а не ввод пользователя.
 
 """
+
+from pprint import pprint
+import re
+
+def get_ip_from_cfg(filename):
+
+    result = {}
+    # regex = ('!.*?interface (\S+).+?'\
+    #     ' ip address (\S+) (\S+).*?!')
+    
+    regex = (r'!.+?interface (\S+).+?'\
+        r'(?: ip address (\S+) (\S+)).+?'\
+        r'|(?: ip address (\S+) (\S+)).+?')
+        # r'(?: ip address (\S+) (\S+))|(?: ip address (\S+) (\S+)).+?')
+        # ' (?:ip address (\S+) (\S+)).+?!')
+            # 'ip address (\S+) (\S+).+?!')
+
+    with open(filename) as file:
+        data = file.read()
+        match = re.finditer(regex, data, re.DOTALL)
+        for m in match:
+            print (m.groups ())
+            result[m.group(1)] = m.group(2,3)
+        print ('\n'+40*'*'+'\n')
+        return result
+
+
+if __name__ == '__main__':
+    pprint (get_ip_from_cfg('config_r2.txt'),sort_dicts=0)
+
+'''
+vagrant@PyNEng: $  [main|✚ 1⚑ 1] 
+02:56 $ /home/vagrant/venv/pyneng-py3-8/bin/python /home/vagrant/CourseDir/pyneng/15_module_re/task_15_1b.py
+('Loopback0', '10.2.2.2', '255.255.255.255', None, None)
+('Tunnel0', '10.0.23.2', '255.255.255.0', None, None)
+('Ethernet0/1', '10.255.2.2', '255.255.255.0', None, None)
+(None, None, None, '10.254.2.2', '255.255.255.0')
+('Ethernet0/2', '10.0.29.2', '255.255.255.0', None, None)
+
+****************************************
+
+{'Loopback0': ('10.2.2.2', '255.255.255.255'),
+ 'Tunnel0': ('10.0.23.2', '255.255.255.0'),
+ 'Ethernet0/1': ('10.255.2.2', '255.255.255.0'),
+ None: (None, None),
+ 'Ethernet0/2': ('10.0.29.2', '255.255.255.0')}
+'''
