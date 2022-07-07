50fd4565010832a11886d4e0d05808e2cd0aacd3
dkasyan <dmitriy.kasyan@gmail.com>
2022-07-07 03:25:47 +0000

    2022-07-07 task_15_2 parse in list

--- 15_module_re/task_15_2.py
+++ 15_module_re/task_15_2.py
@@ -21,3 +21,21 @@
 Проверить работу функции на примере файла sh_ip_int_br.txt.
 
 """
+import re
+
+lines = '''
+Interface                  IP-Address      OK? Method Status                Protocol
+FastEthernet0/0            15.0.15.1       YES manual up                    up
+FastEthernet0/1            10.0.12.1       YES manual up                    up
+'''
+
+regexp =  r'(\S+) +'\
+          r'([\d.]+) +'\
+          r'\w+ \w+ +'\
+          r'(up|down|administratively down) +'\
+          r'(up|down)'
+
+result = re.finditer(regexp,lines)
+
+for match in result:
+  print (match.groups())
